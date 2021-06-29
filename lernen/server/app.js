8f43b1ec108bb326f544779b46e5ec4848f7b736
StalkeRgit <Abdulkarem.alsabbagh@gmail.com>
2021-06-29 15:27:13 +0200

    switch to atlas

--- lernen/server/app.js
+++ lernen/server/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Postrouter = require('../server/routes/posts.routers')
-
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/posts',{useCreateIndex: true,useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false});
+mongoose.connect(`mongodb+srv://abdulkarem:${process.env.ATLAS_PW}@cluster0.7pfq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{useCreateIndex: true,useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false});
 const db = mongoose.connection;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
